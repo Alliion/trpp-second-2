b49a16d99df34209058e74055624146ffc9f61e6
Alina <kruzina.alina@gmail.com>
2021-04-06 01:58:38 +0300

    Initial commit

---
+++ src/main/java/ru/mirea/trppsecond2/controllers/HealthController.java
@@ -0,0 +1,21 @@
+package ru.mirea.trppsecond2.controllers;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import ru.mirea.trppsecond2.entity.HealthResponse;
+
+/** Проверка состояния сервера. */
+@Controller()
+public class HealthController {
+
+    /**
+     * Проверить состояние сервера.
+     * @return ответ
+     * 200 - OK
+     */
+    @Get
+    public HttpResponse<HealthResponse> healthCheck() {
+        return HttpResponse.ok(new HealthResponse("ok"));
+    }
+}
---
+++ src/main/java/ru/mirea/trppsecond2/entity/HealthResponse.java
@@ -0,0 +1,18 @@
+package ru.mirea.trppsecond2.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/** Ответ на healthCheck. */
+@Getter
+@Setter
+//@AllArgsConstructor
+public class HealthResponse {
+
+    /** Статус работы. */
+    private String status;
+
+
+    public HealthResponse(String ok) {
+    }
+}
