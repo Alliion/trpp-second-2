bfbf744347ab514e0c582cd63399598b11af422a
Alina <kruzina.alina@gmail.com>
2021-04-06 02:50:04 +0300

    2nd commit

--- src/main/java/ru/mirea/trppsecond2/entity/HealthResponse.java
+++ src/main/java/ru/mirea/trppsecond2/entity/HealthResponse.java
@@ -1,18 +1,18 @@
 package ru.mirea.trppsecond2.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 /** Ответ на healthCheck. */
 @Getter
 @Setter
-//@AllArgsConstructor
+@AllArgsConstructor
 public class HealthResponse {
 
     /** Статус работы. */
     private String status;
 
 
-    public HealthResponse(String ok) {
-    }
+
 }
--- src/test/java/ru/mirea/TrppTest.java
+++ src/test/java/ru/mirea/TrppTest.java
@@ -2,10 +2,12 @@ package ru.mirea;
 
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+
+
 
 @MicronautTest
 class TrppTest {
